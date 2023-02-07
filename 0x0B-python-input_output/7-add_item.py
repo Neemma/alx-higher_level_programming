c2454deef3cbc2a364a60e062cbb34229b591d45
Bethlydia Mutuba <mutubaneema@gmail.com>
2023-02-07 23:22:14 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""This module creates a script that adds args to python list and saves them"""
+"""
+Module that adds all arguments to a Python list, and then
+save them into a file.
+"""
+import sys
+import os.path
 
-from os import path
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json") is false:
-    save_to_json_file([], "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+file_save = __import__('5-save_to_json_file').save_to_json_file
+file_load = __import__('6-load_from_json_file').load_from_json_file
+
+my_list = []
+if os.path.exists("add_item.json"):
+    my_list = file_load("add_item.json")
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+file_save(my_list, "add_item.json")
